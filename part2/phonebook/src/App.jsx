a176d64ee91bbc780681d5e77256d8b45748369b
jakubosuchowski <osuchowski.jakub@gmail.com>
2025-05-06 14:42:57 +0200

    completed exercises 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,96 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const FilterForm = ({filter, handleFilterField}) => {
+    return (
+        <form>
+            <div>filter shown with<input value={filter} onChange={handleFilterField} /></div>
+        </form>
+    )
+}
+
+const PersonForm = ({name, number, handleNameChange, handlePhoneChange, addNumber}) => {
+    return (
+            <form>
+                <div>name: <input value={name} onChange={handleNameChange} /></div>
+                <div>number: <input value={number} onChange={handlePhoneChange} /></div>
+                <div><button type="submit" onClick={addNumber}>add</button></div>
+            </form>
+    )
+
+}
+
+const Numbers = ({persons}) => {
+    return (
+        persons.map((person => {
+            return (
+                <p key={person.id}>{person.name} {person.number}</p>
+            )
+        }))
+    )
+
+}
+
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
+    const [newName, setNewName] = useState('')
+    const [newPhone, setNewPhone] = useState('')
+    const [filter, setFilter] = useState('')
+
+
+    const filteredPersons = filter ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())) : persons
+
+    const addNumber = (event) => {
+        event.preventDefault()
+
+        const personObject = {
+            name: newName,
+            number: newPhone,
+            id: persons.length + 1,
+        }
+
+         if (persons.some(person => person.name === personObject.name)) {
+             alert(`${personObject.name} is already added to phonebook`)
+         } else {
+             setPersons(persons.concat(personObject))
+         }
+        setNewName('')
+        setNewPhone('')
+
+    }
+
+const handleNameChange = (event) => {
+        setNewName(event.target.value)
+}
+
+
+const handlePhoneChange = (event) => {
+        setNewPhone(event.target.value)
+    }
+
+
+    const handleFilterField = (event) => {
+        setFilter(event.target.value)
+    }
+
+
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <FilterForm value={filter} handleFilterField={handleFilterField} />
+            <h2>add a new</h2>
+            <PersonForm name={newName} number={newPhone} handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange}  addNumber={addNumber} />
+            <h2>Numbers</h2>
+            < Numbers persons={filteredPersons} />
+        </div>
+    )
 }
 
 export default App
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-  </StrictMode>,
 )
