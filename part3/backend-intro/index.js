84e73c2958d9379dd37df6a93b8f34182fb86a73
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-06-02 12:42:58 +0200

    added notes backend

--- part3/backend-intro/index.js
+++ part3/backend-intro/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: "1",
@@ -31,8 +33,49 @@ let notes = [
   app.get('/api/notes/:id', (request, response) => {
     const id = request.params.id
     const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+  })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+  })
+
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+
+return String(maxId + 1)
+  }
+
+    app.post('/api/notes', (request,response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
     response.json(note)
   })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
