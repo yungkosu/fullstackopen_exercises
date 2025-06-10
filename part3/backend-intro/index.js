3125674e6f34216327bec0e72df985b5cb9afed0
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-06-10 16:15:24 +0200

    added backend and db for notes

--- part3/backend-intro/index.js
+++ part3/backend-intro/index.js
@@ -1,27 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.json())
+const Note = require('./models/note')
 
-
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-
-  const requestLogger = (request, response, next) => {
+const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
     console.log('Body:  ', request.body)
@@ -37,19 +20,16 @@ let notes = [
   })
 
   app.get('/api/notes', (request,response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -58,16 +38,6 @@ let notes = [
 
     response.status(204).end()
   })
-
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-
-return String(maxId + 1)
-  }
-
     app.post('/api/notes', (request,response) => {
     const body = request.body
 
@@ -77,18 +47,18 @@ return String(maxId + 1)
         })
     }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId()
-    }
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
 
-    notes = notes.concat(note)
-    response.json(note)
+    note.save().then(savedNote =>  {
+      response.json(savedNote)
+    })
   })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`)
 })
