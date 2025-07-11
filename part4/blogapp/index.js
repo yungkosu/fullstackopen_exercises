219a1a22a1a333045356f2a5b52e6d633e204df9
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-07-11 16:23:51 +0200

    added blog app

---
+++ part4/blogapp/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://osuchowskijakub:0ckiJ1W3AJZ6dWJV@cluster0.gaxxx1g.mongodb.net/blogListApp?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
