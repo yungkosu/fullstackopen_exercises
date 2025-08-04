b1f47dfc03f8527c3cc6838783ed623e09d52463
jakubosuchowski <osuchowski.jakub@gmail.com>
2025-08-04 14:28:17 +0200

    test for POST blog

--- part4/blogapp/tests/test_api.test.js
+++ part4/blogapp/tests/test_api.test.js
@@ -1,4 +1,4 @@
-const {test, after} = require('node:test')
+const { test, after } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -18,17 +18,41 @@ test('the right amount of blogs is returned', async () => {
   assert.strictEqual(response.body.length, 3)
 })
 
-test('blog contains parameter named id', async () => {
+test('each blog contains parameter named id', async () => {
   const response = await api.get('/api/blogs')
-  console.log(response.body);
+  console.log(response.body)
   for (const blog of response.body) {
-    assert.ok('id' in blog, 'Each blog should have an id field');
+    assert.ok('id' in blog, 'Each blog should have an id field')
   }
-  })
+})
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'Kuba',
+    url: 'https://kuba-api.herokuapp.com/api/blogs',
+    likes: 5,
+  }
 
+  // Get initial number of blogs
+  const initialResponse = await api.get('/api/blogs')
+  const initialLength = initialResponse.body.length
 
+  // Add new blog
+  await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
+  // Get updated blogs
+  const updatedResponse = await api.get('/api/blogs')
+  assert.strictEqual(updatedResponse.body.length, initialLength + 1)
+
+  // Check if new blog title exists in response
+  const titles = updatedResponse.body.map(blog => blog.title)
+  assert.ok(titles.includes(newBlog.title), 'New blog title should be in response')
+})
 
 after(async () => {
   await mongoose.connection.close()
