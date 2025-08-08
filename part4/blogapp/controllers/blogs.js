93600823c9ca6f45a5f7506f0955da9aff50e241
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-08-08 14:51:04 +0200

    added a lot of stuff

--- part4/blogapp/controllers/blogs.js
+++ part4/blogapp/controllers/blogs.js
@@ -15,4 +15,30 @@ blogsRouter.post('/', (request, response) => {
   })
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async(request, response, next) => {
+  const { content, author, url, likes } = request.body
+
+  await Blog.findById(request.params.id)
+    .then(blog => {
+      if (!blog) {
+        return response.status(404).end()
+      }
+
+      blog.title = content
+      blog.author = author
+      blog.url = url
+      blog.likes = likes
+
+      return blog.save().then((updatedBlog) => {
+        response.json(updatedBlog)
+      })
+    })
+    .catch(error => next(error))
+})
+
 module.exports = blogsRouter
