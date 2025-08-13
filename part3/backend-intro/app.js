07bc20473d114458424c85937f4e45757ca2d698
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-08-13 14:40:22 +0200

    added jwt token

--- part3/backend-intro/app.js
+++ part3/backend-intro/app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -26,6 +27,8 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
