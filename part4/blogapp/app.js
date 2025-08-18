8cdddae235b6b05180a5242ab13b24dc78362623
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-08-18 16:58:44 +0200

    user creation

--- part4/blogapp/app.js
+++ part4/blogapp/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -16,6 +17,7 @@ mongoose
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 
 module.exports = app
