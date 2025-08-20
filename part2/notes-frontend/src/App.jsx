3bce381e16f5f0eb4337e3d9dbd51dba24870456
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-08-20 16:24:24 +0200

    added login form

--- part2/notes-frontend/src/App.jsx
+++ part2/notes-frontend/src/App.jsx
@@ -10,6 +10,9 @@ const App = () => {
     const [newNote, setNewNote] = useState("")
     const [showAll, setShowAll] = useState(true)
     const [errorMessage, setErrorMessage] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
 
 
     useEffect(() => {
@@ -59,17 +62,40 @@ const toggleImportanceOf = (id) => {
         })
 }
 
+const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with,', username, password)
+}
+
 
     return (
         <div>
             <h1>Notes</h1>
             <Notification message={errorMessage} />
-            <div>
-                <button onClick={() => setShowAll(!showAll)}
-                >
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+</form>
+<button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
                 </button>
-            </div>
             <ul>
                 {notesToShow.map(note =>
                     <Note 
