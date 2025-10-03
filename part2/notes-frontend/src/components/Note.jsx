c2d323bb93074655316c10466a2e21c815bed9e8
Jakub Osuchowski <osuchowski.jakub@gmail.com>
2025-10-03 09:56:10 +0200

    ok

--- part2/notes-frontend/src/components/Note.jsx
+++ part2/notes-frontend/src/components/Note.jsx
@@ -2,10 +2,12 @@ const Note = ({ note, toggleImportance }) => {
     const label = note.important ? 'make not important' : 'make important'
 
 
-    return <li className={'note'}>
-        {note.content}
-        <button onClick={toggleImportance}>{label}</button>
-        </li>
+  return (
+    <li className='note'>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
 }
 
 export default Note
